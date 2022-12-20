d1756ed5c1e7907cebadd6534f7ebdab232c80ab
Shivansh Pandey <99907935+shivpandey02@users.noreply.github.com>
2022-12-20 17:50:17 +0530

    Add files via upload

---
+++ pandey/3.cpp
@@ -0,0 +1,9 @@
+/*Applications of Recursion
+1. Many algorithm techniques are based on recursion
+      i. Dynamic Programming
+      ii. Backracking
+      iii. Divide and Conquer (Binary Search, Quicksort, Merge sort )
+      
+2. Many problems inherntly recursive
+      i.Tower of Hanoi
+      ii. DFS based traversals (DFS graph and Inorder/preorder/ postorder traversal of tree)*/
