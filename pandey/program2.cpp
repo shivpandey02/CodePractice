34cca5332dde169e333a491e11d55f97d3730a8c
Shivansh Pandey <99907935+shivpandey02@users.noreply.github.com>
2022-12-22 08:39:07 +0530

    Add files via upload

---
+++ pandey/program2.cpp
@@ -0,0 +1,20 @@
+// c ++ program to print the size of int,float, double and char
+#include<iostream>
+using namespace std;
+int main(){
+    int a ;
+    float b;
+    double c;
+    char d;
+    cout<<"the size of the int is "<<sizeof(a)<<endl;
+    cout<<"the size of the flaot is "<<sizeof(b)<<endl;
+    cout<<"the size of the double is "<<sizeof(c)<<endl;
+    cout<<"the size of the char is "<<sizeof(d)<<endl;
+
+
+
+
+
+
+    return 0;
+}
