46d2c28a15f8fccf3b17230bd2430bb732c6f58c
Shivansh Pandey <99907935+shivpandey02@users.noreply.github.com>
2023-01-13 10:55:14 +0530

    Add files via upload

---
+++ pandey/question12.java
@@ -0,0 +1,12 @@
+// ascii value 
+public class question12 {
+    public static void main(String[] args) {
+        char ch1 = 'y';
+        char ch2 = 'z';
+        int asciivalue1 = ch1;
+        int asciivalue2 = ch2;
+        System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);
+        System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);
+    }
+
+}
