73e0289a05bf41e7cfadb1a81fc1dc618c001259
Shivansh Pandey <99907935+shivpandey02@users.noreply.github.com>
2023-01-15 21:09:57 +0530

    Add files via upload

---
+++ pandey/question4.java
@@ -0,0 +1,13 @@
+// swap two numbers
+public class question4 {
+    public static void main(String[] args) {
+        int a =14;
+        int b =16;
+        int temp;
+         temp=a;
+         a=b;
+         b=temp;
+         System.out.printf("the numbers after swapping is %d and %d ",a,b);  
+    }
+    
+}
